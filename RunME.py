371add88a0d4ea893290cc376cad6a6bcdb919c7
Prabal Jain <prabaljainn@gmail.com>
2021-08-24 07:06:02 +0530

    Just Run RunME.py to execute GUI Script to Run

--- RunME.py
+++ RunME.py
@@ -1,3 +1,10 @@
+#############################################
+__author__ = "Prabal Jain"                  #
+__LinkedIn__ = "prabaljainn"                #
+__Github__ = "prabaljainn"                  #
+#############################################
+
+
 from Scripts import graphical_script
 if __name__ == '__main__':
     graphical_script.guibuild()
